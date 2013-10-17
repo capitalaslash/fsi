ccd750921068ef90876256ba0f6549a4e4dc5dec
Antonio Cervone <ant.cervone@gmail.com>
2013-10-17 17:51:31 +0200

    move to vel-pressure formulation

--- fenics/structure_seg.py
+++ fenics/structure_seg.py
@@ -1,6 +1,10 @@
 #! /usr/bin/python2
 
-dim = 3
+output_dir = 'output/seg'
+
+#mesh_file = 'hex2.xml'
+mesh_file = None
+dim = 2
 
 tin = 0.
 tout = 0.25
@@ -16,6 +20,7 @@ nz = 1
 E = 1e8
 ni = 0.3
 
+import os
 from dolfin import *
 
 def structure():
@@ -27,9 +32,11 @@ def structure():
   #mesh = UnitSquareMesh(n,n,"crossed")
   #mesh = UnitCubeMesh(4,4,4)
   #mesh = Mesh("beam2d.xml")
-  if(dim == 2):
+  if mesh_file is not None:
+   mesh = Mesh(mesh_file)
+  if dim == 2:
     mesh = RectangleMesh(0., 0., lx, ly, nx, ny)
-  elif(dim == 3):
+  elif dim == 3:
     mesh = BoxMesh(0., 0., 0., lx, ly, lz, nx, ny, nz)
 
   V = VectorFunctionSpace( mesh, 'CG', 2);
@@ -72,17 +79,23 @@ def structure():
 
   mu = Constant( E / 2*(1+ni) )
   lmbda = Constant( E*ni / (1+ni)*(1-2*ni) )
+  ilambda = 1. / lmbda
 
   def eps(v):
-    return 0.5*(grad(v)+grad(v).T)
+    return sym(grad(v))
 
   def sigma(v):
     return 2.0*mu*eps(v) + lmbda*tr(eps(v))*I
 
   a_d = dot(d,b)*dx
+  #a_u = dot(u,v)*dx \
+      #+ dt*inner(sigma(dt*u),eps(v))*dx \
+      #+ dt*p*q*dx
   a_u = dot(u,v)*dx \
-      + dt*inner(sigma(dt*u),eps(v))*dx \
-      + dt*p*q*dx
+      + dt*2.*mu*inner(eps(dt*u),eps(v))*dx \
+      + dt*p*div(v)*dx \
+      + dt*q*div(u)*dx \
+      - ilambda*p*q*dx
 
   A_d = assemble(a_d)
   A_u = assemble(a_u)
@@ -98,19 +111,27 @@ def structure():
   #solver_d.set_operator(A_d)
   #solver_d.parameters['monitor_convergence']=True
 
-  file_d = File('d_seg.pvd')
-  file_u = File('u_seg.pvd')
+  if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+  file_d = File(output_dir + '/d.pvd')
+  file_u = File(output_dir + '/u.pvd')
+  file_p = File(output_dir + '/p.pvd')
 
   sol = Function(W)
   t = tin
   file_d << (d_old,t)
   file_u << (u_old,t)
+  file_p << (p_old,t)
   while t + dt < tout+DOLFIN_EPS:
     t += dt
     print "time = %f" % t
 
+    #L_u = dot(u_old+dt*f,v)*dx \
+        #- dt*inner(sigma(d_old),eps(v))*dx
     L_u = dot(u_old+dt*f,v)*dx \
-        - dt*inner(sigma(d_old),eps(v))*dx
+        - dt*2.*mu*inner(eps(d_old),eps(v))*dx \
+        - div(d_old)*q*dx
+
     rhs_u = assemble(L_u)
     bc_u.apply(rhs_u)
 
@@ -125,6 +146,7 @@ def structure():
 
     file_d << (d_old,t)
     file_u << (u_old,t)
+    file_p << (p_old,t)
 
 if __name__ == '__main__':
   structure()
