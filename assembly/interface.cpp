d9dca14311039ad7e9a459a951a328fda2b7e3ba
Antonio Cervone <ant.cervone@gmail.com>
2014-02-11 11:11:38 +0100

    add InterfaceSystem

--- assembly/interface.cpp
+++ assembly/interface.cpp
@@ -1,5 +1,6 @@
 #include "interface.hpp"
 
+#include <libmesh/equation_systems.h>
 #include <libmesh/explicit_system.h>
 #include <libmesh/dof_map.h>
 #include <libmesh/fe.h>
@@ -8,40 +9,25 @@
 
 using namespace libMesh;
 
-void assemble_interface( EquationSystems & es, std::string const & name )
+void InterfaceSystem::assemble()
 {
-    if (!(name == "interface"))
-    {
-        libmesh_error();
-    }
+    EquationSystems const & es = get_equation_systems();
 
-    const MeshBase& mesh = es.get_mesh();
+    const MeshBase& mesh = get_mesh();
 
-//    const uint dim = mesh.mesh_dimension();
-
-    ExplicitSystem & sys = es.get_system<ExplicitSystem> (name);
-
-    const uint var = sys.variable_number(name);
-
-    const DofMap& dof_map = sys.get_dof_map();
-
-//    FEType fe_type = sys.variable_type(var);
-
-//    AutoPtr<FEBase> fe (FEBase::build(dim, fe_type));
-//    QGauss qrule (dim, fe_type.default_quadrature_order());
-//    fe->attach_quadrature_rule (&qrule);
+    const DofMap& dof_map = this->get_dof_map();
 
     std::vector<dof_id_type> dof_indices_var;
 
     DenseVector<Real> elem_value;
 
-    uint const flag_s = es.parameters.get<uint>("flag_s");
-    uint const flag_f = es.parameters.get<uint>("flag_f");
+    subdomain_id_type const flag_s = es.parameters.get<subdomain_id_type>("flag_s");
+    subdomain_id_type const flag_f = es.parameters.get<subdomain_id_type>("flag_f");
 
     AutoPtr<NumericVector<Real> > on_solid (NumericVector<Real>::build(es.comm()));
     AutoPtr<NumericVector<Real> > on_fluid (NumericVector<Real>::build(es.comm()));
-    on_solid->init( sys.n_dofs(), sys.n_local_dofs(), false, PARALLEL );
-    on_fluid->init( sys.n_dofs(), sys.n_local_dofs(), false, PARALLEL );
+    on_solid->init( this->n_dofs(), this->n_local_dofs(), false, PARALLEL );
+    on_fluid->init( this->n_dofs(), this->n_local_dofs(), false, PARALLEL );
 
     MeshBase::const_element_iterator       el     = mesh.active_local_elements_begin();
     const MeshBase::const_element_iterator end_el = mesh.active_local_elements_end();
@@ -50,9 +36,7 @@ void assemble_interface( EquationSystems & es, std::string const & name )
     {
         const Elem* elem = *el;
 
-//        fe->reinit(elem);
-
-        dof_map.dof_indices (elem, dof_indices_var, var);
+        dof_map.dof_indices (elem, dof_indices_var, 0);
 
         elem_value.resize(dof_indices_var.size());
         for( uint k=0; k < dof_indices_var.size(); k++)
@@ -83,14 +67,14 @@ void assemble_interface( EquationSystems & es, std::string const & name )
     for( ; el != end_el; ++el )
     {
         const Elem* elem = *el;
-        dof_map.dof_indices (elem, dof_indices_var, var);
+        dof_map.dof_indices (elem, dof_indices_var, 0);
 
         elem_value.resize(dof_indices_var.size());
         for( uint k=0; k < dof_indices_var.size(); k++)
         {
             dof_id_type const current_dof = dof_indices_var[k];
-            if((sys.solution->first_local_index() <= current_dof) &&
-                    (current_dof < sys.solution->last_local_index()))
+            if((this->solution->first_local_index() <= current_dof) &&
+                    (current_dof < this->solution->last_local_index()))
             {
                 bool const node_on_solid = (*on_solid)(current_dof) > 0.;
                 bool const node_on_fluid = (*on_fluid)(current_dof) > 0.;
@@ -103,9 +87,81 @@ void assemble_interface( EquationSystems & es, std::string const & name )
                 }
             }
         }
-        sys.solution->add_vector(elem_value, dof_indices_var);
+        this->solution->add_vector(elem_value, dof_indices_var);
     }
 
-    sys.solution->close();
-    sys.update();
+    this->solution->close();
+    this->update();
+
+    M_isAssembled = true;
+
+}
+
+void InterfaceSystem::mark_facets(subdomain_id_type const /*flag*/)
+{
+    libmesh_error();
+//        const MeshBase& mesh = get_mesh();
+//        uint const dim = mesh.mesh_dimension();
+
+//        FEType fe_type = variable_type(0);
+
+//        AutoPtr<FEBase> fe_face (FEBase::build(dim, fe_type));
+//        QGauss qface(dim-1, fe_type.default_quadrature_order());
+
+//        MeshBase::const_element_iterator       el     = mesh.active_local_elements_begin();
+//        const MeshBase::const_element_iterator end_el = mesh.active_local_elements_end();
+
+//        for ( ; el != end_el; ++el)
+//        {
+//            const Elem* elem = *el;
+//            for (uint s=0; s<elem->n_sides(); s++)
+//            {
+//                AutoPtr<Elem> side (elem->build_side(s));
+
+//                const std::vector<std::vector<Real> >&  phi_face = fe_face->get_phi();
+//                const std::vector<Real>& JxWface = fe_face->get_JxW();
+
+//                fe_face->reinit(elem, s);
+
+//                for (uint qp=0; qp<qface.n_points(); qp++)
+//                {
+//                    for (uint i=0; i<phi_face.size(); i++)
+//                    {
+//                    }
+//                }
+
+//            }
+//        }
+}
+
+Real InterfaceSystem::length() const
+{
+    Real length = 0;
+    const EquationSystems& es = get_equation_systems();
+    const MeshBase& mesh = get_mesh();
+    boundary_id_type const flag_int = es.parameters.get<boundary_id_type>("flag_int");
+    subdomain_id_type const flag_s = es.parameters.get<subdomain_id_type>("flag_s");
+
+    MeshBase::const_element_iterator       el     = mesh.active_local_elements_begin();
+    const MeshBase::const_element_iterator end_el = mesh.active_local_elements_end();
+
+    for ( ; el != end_el; ++el)
+    {
+        Elem const* elem = *el;
+        if(elem->subdomain_id() == flag_s)
+        {
+            for (uint s=0; s<elem->n_sides(); s++)
+            {
+                AutoPtr<Elem const> side (elem->build_side(s));
+                if(mesh.boundary_info->has_boundary_id(elem, s, flag_int))
+                {
+                    length += side->length(0,1);
+                }
+            }
+        }
+    }
+
+    comm().sum(length);
+
+    return length;
 }
--- assembly/interface.hpp
+++ assembly/interface.hpp
@@ -4,8 +4,57 @@
 #include "FSIconfig.h"
 
 #include <libmesh/libmesh.h>
-#include <libmesh/equation_systems.h>
+#include <libmesh/explicit_system.h>
+#include <libmesh/numeric_vector.h>
+#include <libmesh/dof_map.h>
 
-void assemble_interface( libMesh::EquationSystems & es, std::string const & name );
+class InterfaceSystem: public ExplicitSystem
+{
+public:
+
+    InterfaceSystem (EquationSystems& es,
+                     const std::string& name,
+                     const unsigned int number):
+        ExplicitSystem(es, name, number),
+        M_isAssembled(false),
+        M_facetsMarked(false)
+    {
+        this->add_variable("interface", SECOND, LAGRANGE);
+    }
+
+    // build interface position in solution vector
+    void assemble();
+
+    // mark facets on interface with given flag
+    void mark_facets(subdomain_id_type const flag);
+    
+    // compute total length of the interface
+    Real length() const;
+
+    // set a bool vector to know if dofs on element are on interface
+    void on_interface( Elem const* e, std::vector<bool>& on_int) const;
+
+private:
+    bool M_isAssembled;
+    bool M_facetsMarked;
+    // boundary_id_type const M_fluidFlag;
+    // boundary_id_type const M_solidFlag;
+};
+
+inline void InterfaceSystem::on_interface( Elem const* e, std::vector<bool>& on_int) const
+{
+    std::vector<dof_id_type> dof_indices;
+    get_dof_map().dof_indices(e, dof_indices, 0);
+
+    on_int.resize(dof_indices.size(), false);
+
+    for(uint i = 0; i < dof_indices.size(); i++)
+    {
+        if((solution->first_local_index() <= dof_indices[i]) &&
+                (dof_indices[i] < solution->last_local_index()) &&
+                ((*solution)(dof_indices[i]) > 0.5) )
+            on_int[i] = true;
+    }
+}
 
 #endif // INTERFACE_HPP
