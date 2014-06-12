39a7e1d569900065a86cc48269e5fe04793c4176
Antonio Cervone <ant.cervone@gmail.com>
2014-06-12 16:15:44 +0200

    add global FSI.hpp config file

---
+++ FSI.hpp
@@ -0,0 +1,12 @@
+#ifndef FSI_HPP
+#define FSI_HPP
+
+#include "FSIconfig.h"
+
+#include <libmesh/libmesh.h>
+
+using libMesh::Real;
+using libMesh::subdomain_id_type;
+using libMesh::dof_id_type;
+
+#endif //FSI_HPP
